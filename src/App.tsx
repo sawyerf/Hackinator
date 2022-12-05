3bc06e2020bd71df72bf537fa5c7fe43b2b1bbf5
owalid <othmane.el.ayadi@gmail.com>
2022-12-06 00:10:00 +0100

    feat: add horizontal drag

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import diagramData from './data/diagram.json'
 import { BlockNodeType, BlockType } from './data/diagram';
 import Diagram from './components/Diagram';
 import Information from './components/Information';
+import ListNextNode from './components/ListNextNode';
+import ScrollContainer from 'react-indiana-drag-scroll'
 
 
 const App = () => {
@@ -10,10 +12,12 @@ const App = () => {
   const [ node,  setNode ] = useState<BlockNodeType>(data)
 
   return (
-    <div className="App">
-      <Diagram diagramData={data} setSelect={setNode}/>
-      <Information node={node} setSelect={setNode}/>
-    </div>
+    <ScrollContainer className="scroll-container">
+      <div className="App">
+        <Diagram diagramData={data} setSelect={setNode}/>
+        <Information node={node} setSelect={setNode}/>
+      </div>
+    </ScrollContainer>
   )
 }
 
