ebc66887d9b7e38d87d68ad8783dffbb5eeef27c
owalid <othmane.el.ayadi@gmail.com>
2022-12-12 01:04:31 +0100

    fix wheel zoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState } from 'react';
 import diagramData from './data/diagram.json'
-import { BlockNodeType, BlockType } from './data/diagram';
+import { BlockNodeType } from './data/diagram';
 import Diagram from './components/Diagram';
 import Information from './components/Information';
-import ListNextNode from './components/ListNextNode';
 import ScrollContainer from 'react-indiana-drag-scroll'
-import { Space } from 'react-zoomable-ui';
-
-
 
 const App = () => {
 	const data: BlockNodeType = diagramData as BlockNodeType;
@@ -15,9 +11,7 @@ const App = () => {
   return (
     <div className="App">
       <ScrollContainer className="scroll-container">
-        <Space>
-          <Diagram diagramData={data} setSelect={setNode}/>
-        </Space>
+        <Diagram diagramData={data} setSelect={setNode}/>
       </ScrollContainer>
       <Information node={node} setSelect={setNode}/>
     </div>
--- src/components/Diagram.tsx
+++ src/components/Diagram.tsx
@@ -3,7 +3,7 @@ import NodeBlock from './NodeBlock';
 import TextBlock from './TextBlock';
 import UrlBlock from './UrlBlock';
 import { BlockNodeType, BlockTextType, BlockType, BlockUrlType } from '../data/diagram';
-
+import { Space } from 'react-zoomable-ui';
 
 type DigriamProps = {
 	diagramData: BlockNodeType;
@@ -57,12 +57,14 @@ const Diagram = (props: DigriamProps) => {
 
 	return (
 		<div className='diagram'>
-			<div className='parent'>
-				<div className='child'>
-					{typeIdentifier(props.diagramData)}
-				</div>
-			</div>
-		</div >
+      <Space>
+        <div className='parent'>
+          <div className='child'>
+            {typeIdentifier(props.diagramData)}
+          </div>
+        </div>
+      </Space>
+		</div>
 	);
 }
 
